25257ffd6f8caddcdc0c57abb6aa7be46c66156b
Divyansh Ojha <91757380+divyanshojha99@users.noreply.github.com>
2021-11-16 20:41:52 +0530

    Update highlowgame.cpp

--- highlowgame.cpp
+++ highlowgame.cpp
@@ -1,10 +1,6 @@
 //High/Low Game By Divyansh..
-
-#include<iostream.h>
-#include<conio.h>
-#include<stdlib.h>
-#include<process.h>
-#include<dos.h>
+using namespace std;
+#include <bits/stdc++.h>
 
 void main()
 {
@@ -15,10 +11,10 @@ textcolor(RED);
 
  char ch,a[20],ch2;
  int num=100,rnum,guess,count,ch1,c=0;
- cout<<“nttt******************************”<<“nttt******************************”;
- cout<<“nttt**                          **”<<“nttt** Welcome To High/Low Game **”<<“nttt** ________________________ **”;
- cout<<“nttt**                          **”<<“nttt******************************”<<“nttt******************************”;
- cout<<“nnnn————“<<“n Main  Menu”<<“n————“<<“nnn 1.Rulesnn 2.Playnn 3.Exit”<<“nn Enter your choice(1-3):”;
+ cout<<â€œnttt******************************â€<<â€œnttt******************************â€;
+ cout<<â€œnttt**                          **â€<<â€œnttt** Welcome To High/Low Game **â€<<â€œnttt** ________________________ **â€;
+ cout<<â€œnttt**                          **â€<<â€œnttt******************************â€<<â€œnttt******************************â€;
+ cout<<â€œnnnnâ€”â€”â€”â€”â€œ<<â€œn Main  Menuâ€<<â€œnâ€”â€”â€”â€”â€œ<<â€œnnn 1.Rulesnn 2.Playnn 3.Exitâ€<<â€œnn Enter your choice(1-3):â€;
  cin>>ch1;
 
  switch(ch1)
@@ -26,18 +22,18 @@ textcolor(RED);
   case 1:
     {
     clrscr();
-    cout<<“nn———————“<<“n General Description”<<“n———————“;
-    cout<<“nn1.The Computer picks a random number from 0 to 99.n  You must try to guess the number.”;
-    cout<<“nn2.The Computer ask you to enter your guess.You have 8n  chances to guess the number.”;
-    cout<<“nn3.The computer checks the number,if your guess isn  less than computer’s numer than it will show a messagen  that your guess is low and ask for higher number.”;
-    cout<<“nn4.If your guess is higher than the computer’s numbern  than computer will show a message that your guess is high andn  ask for lower number.”;
-    cout<<“nn5.If your guess is equal to the computer’s number,thann  computer will print a message congratulating youn  and will ask you to play the game again or exit.”;
-    cout<<“nn6.If you are unable to guess the number than the computern  will a sorry message and ask you to play the game again or exit.”;
-    D: cout<<“nnn  Press * to goto Main Menu:”;
+    cout<<â€œnnâ€”â€”â€”â€”â€”â€”â€”â€œ<<â€œn General Descriptionâ€<<â€œnâ€”â€”â€”â€”â€”â€”â€”â€œ;
+    cout<<â€œnn1.The Computer picks a random number from 0 to 99.n  You must try to guess the number.â€;
+    cout<<â€œnn2.The Computer ask you to enter your guess.You have 8n  chances to guess the number.â€;
+    cout<<â€œnn3.The computer checks the number,if your guess isn  less than computerâ€™s numer than it will show a messagen  that your guess is low and ask for higher number.â€;
+    cout<<â€œnn4.If your guess is higher than the computerâ€™s numbern  than computer will show a message that your guess is high andn  ask for lower number.â€;
+    cout<<â€œnn5.If your guess is equal to the computerâ€™s number,thann  computer will print a message congratulating youn  and will ask you to play the game again or exit.â€;
+    cout<<â€œnn6.If you are unable to guess the number than the computern  will a sorry message and ask you to play the game again or exit.â€;
+    D: cout<<â€œnnn  Press * to goto Main Menu:â€;
     cin>>ch2;
-    if(ch2==’*’)
+    if(ch2==â€™*â€™)
     {
-     ch2=’#’;
+     ch2=â€™#â€™;
      goto C;
     }
     else
@@ -48,86 +44,81 @@ textcolor(RED);
  case 2:
   {
    clrscr();
-   cout<<“nnnEnter Your Name:”;
+   cout<<â€œnnnEnter Your Name:â€;
    cin>>a;
-   cout<<“nn”;
+   cout<<â€œnnâ€;
    for(int i=0;i<=100;i+=20)
    {
-    cout<<“Loading…….”<<i<<“%r”;
+    cout<<â€œLoadingâ€¦â€¦.â€<<i<<â€œ%râ€;
     sleep(1);
    }
-   cout<<“nnHi “<<a<<“………!!!!”;
+   cout<<â€œnnHi â€œ<<a<<â€œâ€¦â€¦â€¦!!!!â€;
 
 
        A: if(c>0)
     clrscr();
    randomize();
    rnum=random(num);
-   cout<<“nnEnter Your Guess:”;
+   cout<<â€œnnEnter Your Guess:â€;
    cin>>guess;
    count=8;
 
      B: while(guess!=rnum)
         {
-    count–;
+    countâ€“;
     if(guess>rnum)
     {
-     cout<<“nn”<<guess<<” is High….!!!!aa”<<“nnNow You Have “<<count<<” chance left”;
+     cout<<â€œnnâ€<<guess<<â€ is Highâ€¦.!!!!aaâ€<<â€œnnNow You Have â€œ<<count<<â€ chance leftâ€;
      if(count==0)
      break;
-     cout<<“nnEnter Your Guess Again:”;
+     cout<<â€œnnEnter Your Guess Again:â€;
      cin>>guess;
      goto B;
     }
     if(guess<rnum)
     {
-     cout<<“nn”<<guess<<” is Low…!!!aa”<<“nnNow You Have “<<count<<” chance left”;
+     cout<<â€œnnâ€<<guess<<â€ is Lowâ€¦!!!aaâ€<<â€œnnNow You Have â€œ<<count<<â€ chance leftâ€;
      if(count==0)
       break;
-     cout<<“nnEnter Your Guess Again:”;
+     cout<<â€œnnEnter Your Guess Again:â€;
      cin>>guess;
      goto B;
     }
    }
    if(guess==rnum)
    {
-    cout<<“nnttCongratulation “<<a<<” You Have Done IT…….!!!!!aa”;
+    cout<<â€œnnttCongratulation â€œ<<a<<â€ You Have Done ITâ€¦â€¦.!!!!!aaâ€;
     switch(count)
     {
-     case 7: cout<<“nnYour Score is 100 out of 100”;
+     case 7: cout<<â€œnnYour Score is 100 out of 100â€;
       break;
-     case 6: cout<<“nnYour Score is 85 out of 100”;
+     case 6: cout<<â€œnnYour Score is 85 out of 100â€;
       break;
-     case 5: cout<<“nnYour Score is 70 out of 100”;
+     case 5: cout<<â€œnnYour Score is 70 out of 100â€;
       break;
-     case 4: cout<<“nnYour Score is 55 out of 100”;
+     case 4: cout<<â€œnnYour Score is 55 out of 100â€;
       break;
-     case 3: cout<<“nnYour Score is 40 out of 100”;
+     case 3: cout<<â€œnnYour Score is 40 out of 100â€;
       break;
-     case 2: cout<<“nnYour Score is 25 out of 100”;
+     case 2: cout<<â€œnnYour Score is 25 out of 100â€;
       break;
-     case 1: cout<<“nnYour Score is 10 out of 100”;
+     case 1: cout<<â€œnnYour Score is 10 out of 100â€;
       break;
     }
    }
    else
-    cout<<“nnttSorry “<<a<<” Bad Luck……!!! Try Next Timeaaa”;
-   cout<<“nnWant to Play More(y/n):”;
+    cout<<â€œnnttSorry â€œ<<a<<â€ Bad Luckâ€¦â€¦!!! Try Next Timeaaaâ€;
+   cout<<â€œnnWant to Play More(y/n):â€;
    cin>>ch;
 
-   if(ch==’y’||ch==’Y’)
+   if(ch==â€™yâ€™||ch==â€™Yâ€™)
    {
     c++;
     goto A;
    }
    else
     {
-     cout<<“nnnnttt    @@@@@@@@ THANK YOU @@@@@@@@   nn”;
-     cout<<“================================================================================”;
-
-     cout<<“aanntttt Special Thanks Tontttt___________________nntttt  Mrs.Rakhi Jain”;
-     cout<<“nnnntttt Game Developed Byntttt___________________nntttt  Neeraj Mishra”;
-     cout<<“nn================================================================================”;
+     cout<<â€œnnnnttt    @@@@@@@@ THANK YOU @@@@@@@@   nnâ€;
      delay(10000);
      exit(0);
     }
@@ -135,10 +126,7 @@ textcolor(RED);
    break;
  case 3:
   {
-   cout<<“nnn================================================================================”;
-    cout<<“nntttt Special Thanks Tontttt___________________nntttt  Mrs.Rakhi Jain”;
-    cout<<“nnnntttt Game Developed Byntttt___________________nntttt  Neeraj Mishra”;
-    cout<<“nnn================================================================================”;
+   cout<<â€œnnn================================================================================â€;
     delay(10000);
     exit(0);
 
